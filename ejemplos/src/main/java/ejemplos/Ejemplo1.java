15b5e0d086e6ae4fbae7b285673cf6fb7d46c3a5
modemm3 <modestom3@gmail.com>
2021-07-20 20:45:06 -0500

    Add files via upload
    Carga inicial

---
+++ ejemplos/src/main/java/ejemplos/Ejemplo1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+/**
+ *
+ * @author Modesto Mejia
+ */
+public class Ejemplo1 {
+
+    int i=20;
+    static int j=30;
+    
+    public static void main(String []args) {
+        int x=10;
+        System.out.println("Hola Mundo!!"+ x);
+        System.out.print("de nuevo!!");
+    }
+
+    public void miMetodo() {
+        System.out.println("Valor i"+ i);
+    }
+    
+}
---
+++ ejemplos/src/main/java/ejemplos/TiposDeDatos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+/**
+ *
+ * @author Modesto Mejia
+ */
+public class TiposDeDatos {
+    public static void main(String []args) {
+        
+        byte myByte = 126;
+        short myShort = 300;
+        int myInt = 2147483647;
+        System.out.println("Mi valor:" + (char)myByte);
+        System.out.println("Mi valor:" + myShort);
+    
+    }
+    
+}
